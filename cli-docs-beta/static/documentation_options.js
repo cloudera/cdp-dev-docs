ddc8ef3eec682086ecf2ea4b55b3d62d5a9bb177
Cloudera <dev-kitchen@cloudera.com>
2021-07-07 17:32:45 -0700

    CDP Beta CLI Reference Documentation 0.9.38-3-g5268f6c

--- cli-docs-beta/static/documentation_options.js
+++ cli-docs-beta/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.36 (BETA)',
+    VERSION: '0.9.38+3.g5268f6c (BETA)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
