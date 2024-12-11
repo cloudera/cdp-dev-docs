7494d8b0199f716eaf24c4ebadead58aa4ee0d04
SRE Service <sre-svc@cloudera.com>
2024-12-11 14:26:35 -0800

    CDP Beta CLI Reference Documentation 0.9.128

--- cli-docs-beta/static/documentation_options.js
+++ cli-docs-beta/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.127 (BETA)',
+    VERSION: '0.9.128 (BETA)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
